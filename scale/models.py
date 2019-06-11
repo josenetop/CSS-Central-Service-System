016175d48aaf79811778c1a36be6354eef4ebf44
José Neto <neto.jpaulo@gmail.com>
2019-06-11 03:08:45 -0300

    ++Instaled API

--- scale/models.py
+++ scale/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from datetime import timezone
 
 # Create your models here.
+# class Scale(models.Model):
+#     entrada = models.DateTimeField(auto_now_add=False, null=True, blank=True)
+#     saida = models.DateTimeField(auto_now_add=False, null=True, blank=True)
+#     nr17_1 = models.DateTimeField(auto_now_add=False, null=True, blank=True)
+#     lanche = models.DateTimeField(auto_now_add=False, null=True, blank=True)
+#     nr17_2 = models.DateTimeField(auto_now_add=False, null=True, blank=True)
+#     #usuario = model.Foreing
+#     data = models.DateField(auto_now_add=True, null=True, blank=True)
--- scripts/models.py
+++ scripts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from datetime import timezone
 
 # Create your models here.
+class Scripts(models.Model):
+    dataCadastro = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    titulo = models.CharField(max_length=250, null=True, blank=True)
+    texto = models.TextField()
+
+    def __str__(self):
+        return self.titulo
